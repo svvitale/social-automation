65b797101f2079638af3b26276f314dd1ca66f4f
Scott Vitale <svvitale@gmail.com>
2016-07-14 12:37:32 -0600

    Fix a couple syntax errors

--- social_automation/settings.py
+++ social_automation/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import socket
+from enum import Enum
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -50,7 +53,7 @@ INSTALLED_APPS = [
 	'twitter',
 	'facebook',
 	'linkedin'
-)
+]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
