5bf97a75596cbb836a246eded6709c2f2b15fbb8
Scott Vitale <svvitale@gmail.com>
2016-08-23 15:26:12 -0600

    Make the Twitter auth pipeline a little smarter - if a user is already logged in, don't prompt them for an email address.

--- social_automation/auth_pipeline.py
+++ social_automation/auth_pipeline.py
@@ -3,15 +3,20 @@ from social.pipeline.partial import partial
 
 
 @partial
-def get_email_for_twitter_acct(backend, response, details, *args, **kwargs):
+def get_email_for_twitter_acct(backend, response, details, user, *args, **kwargs):
     if backend.name == 'twitter':
         data = backend.strategy.request_data()
 
-        if not data.get('email'):
+        email_param = data.get('email')
+
+        if user.is_authenticated():
+            details['email'] = user.email
+        elif email_param:
+            details['email'] = email_param
+        else:
             return render_to_response('prompt_for_email.html', {
                 'name': response.get('name')
             })
 
-        # Update the user's details with the email address provided
-        details['email'] = data.get('email')
+        # Update the user's details with the email address
         return {'details': details}
