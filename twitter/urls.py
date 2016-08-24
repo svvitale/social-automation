f946bc737a0ee124b5f4f3869c49b604c43760b3
Scott Vitale <svvitale@gmail.com>
2016-08-23 22:46:12 -0600

    First twitter API to follow all twitter users found on a web page

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = []
+from django.conf.urls import url
+from .views import FollowFromUrlView
+
+urlpatterns = [
+    url('follow-from-url/?', FollowFromUrlView.as_view()),
+]
--- twitter/views.py
+++ twitter/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+import re
 
-# Create your views here.
+import requests
+from django.views.generic import View
+
+from social_automation.decorators import json
+from social_automation.responses import APIResponse
+from .twitter import TwitterApi
+
+
+class FollowFromUrlView(View):
+    @json
+    def post(self, request, json=None):
+
+        twitter = TwitterApi(request.user)
+
+        response = requests.get(json['url'])
+
+        if response.status_code != requests.codes.ok:
+            return APIResponse("Requested URL could not be loaded", status=response.status_code)
+
+        follow_set = set()
+
+        # Search for all URLs resembling a twitter user
+        for match in re.finditer(r'http(?:s)?://(?:www\.)?twitter\.com/([\w]+)', response.text):
+
+            # Get just the twitter handle
+            screen_name = match.group(1)
+
+            # Skip duplicates
+            if screen_name in follow_set:
+                continue
+
+            # Follow the user on twitter
+            response = twitter.post(
+                url='https://api.twitter.com/1.1/friendships/create.json',
+                params={
+                    'screen_name': screen_name,
+                    'follow': 'true'
+                }
+            )
+
+            # Process the response to verify that it worked
+            # 200 - Success
+            # 403 - User does not exist
+            if response.status_code not in (requests.codes.ok, requests.codes.forbidden):
+                return APIResponse("Error following user: {0}".format(screen_name), status=response.status_code)
+
+            # Add this handle to our followed set
+            follow_set.add(screen_name)
+
+        # Tell the user what we did
+        return {
+            'msg': 'Found and followed {0} twitter accounts'.format(len(follow_set))
+        }
